8433f4df657508e02d26cbc7d19cfb2f85d69f4a
Mehmet Almas <m.almas63@gmail.com>
2023-11-28 22:51:28 +0100

    281123-3

--- src/main/java/selenium/tekrarlar/M03_DriverManageMethodlari.java
+++ src/main/java/selenium/tekrarlar/M03_DriverManageMethodlari.java
@@ -1,7 +1,12 @@
 package selenium.tekrarlar;
 
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class M03_DriverManageMethodlari {
     public static void main(String[] args) {
@@ -9,8 +14,59 @@ public class M03_DriverManageMethodlari {
         System.getProperty("webdriver.edge.driver" ,"src/resources/edgedriver_win64/msedgedriver.exe");
         WebDriver driver = new EdgeDriver();
 
+        driver.get("https://www.amazon.de/ref=nav_logo");
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        //Sürücünün ögeyi ararken beklemesi gereken süre //örtülü bekleme zaman aşımı değeri
+
+       /*
+        *Implicit wait : Selenium WebDriver'a, sayfada öğeyi bulamadığında istisna atmadan önce belirli bir
+         süre beklemesini (süreler asagidaki gibi belirlenebilir) söyleriz.
+
+        * driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
+        * driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(25));
+        * driver.manage().timeouts().implicitlyWait(Duration.ofMinutes(5));
+        * driver.manage().timeouts().implicitlyWait(Duration.ofHours(24));
+        * driver.manage().timeouts().implicitlyWait(Duration.ofDays(2));
+        * driver.manage().timeouts().implicitlyWait(Duration.ofNanos(1));
+
+        * driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); // Deprecated
+
+        */
+
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
+        // Bir hata vermeden önce sayfa yüklemesinin tamamlanması için beklenecek süre, sayfa yükleme zaman aşımının değeri
+        driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(15));
+        //Eşzamansız bir komut dosyasının hata vermeden önce yürütmeyi bitirmesi için beklenecek süre, kod zaman aşımı değeri
 
 
+
+        driver.manage().timeouts().getImplicitWaitTimeout();   // direk yazdirirsan PT5S bunu verir
+        System.out.println(driver.manage().timeouts().getImplicitWaitTimeout().getSeconds()); // 5 yukarida biz atadik
+
+        driver.manage().timeouts().getPageLoadTimeout();  //direk yazdirirsan PT5M bunu verir
+        System.out.println(driver.manage().timeouts().getPageLoadTimeout().getSeconds());       // 15 varsayilan saniye degeri
+
+        driver.manage().timeouts().getScriptTimeout();  //direk yazdirirsan PT30S bunu verir
+        System.out.println(driver.manage().timeouts().getScriptTimeout().getSeconds());         // 15  varsayilan saniye degeri
+
+        //---------------------------------------------------------------------------
+
+
+        driver.manage().window().fullscreen(); // sayfayi tam ekran yapar
+        driver.manage().window().maximize();  // ekrani tüm sayfaya kaplatir (ekran boyutlari her bilgisayarda farkli olabilir)
+        driver.manage().window().minimize();  // sayfayi ekrandan asagiya indirir, minimize eder
+
+        driver.manage().window().getPosition(); // ekranin o anki durdugu pozisyonunu(yeri) getirir (en-boy farkli olacak sekilde ayni pozisyonda sayfa olabilir)
+        System.out.println( driver.manage().window().getPosition()); // (10, 10)
+
+        driver.manage().window().getSize(); // ekranin o anki en boy bilgilerini getirir
+        System.out.println(driver.manage().window().getSize());  // (945, 1020)
+
+        driver.manage().window().setPosition(new Point(100,100)); // yeni bir pozisyon verdik
+        driver.manage().window().setSize(new Dimension(1000,750));  // yeniden boyutlandirdik
+
+        //---------------------------------------------------------------------------
 
 
 
---
+++ src/main/java/selenium/tekrarlar/M05_DriverfindElemenMetodu.java
@@ -0,0 +1,52 @@
+package selenium.tekrarlar;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import java.time.Duration;
+
+public class M05_DriverfindElemenMetodu {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.edge.driver", "src/resources/edgedriver_win64/msedgedriver.exe");
+        WebDriver driver = new EdgeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+
+        driver.get("https://www.amazon.de/ref=nav_logo");
+
+/*
+        By ByAll = null;
+
+        //web sitesindeki elementleri almak icin kullanilir.
+
+        driver.findElement(new By.ById("frame")); // String id aliyor
+        driver.findElement(new By.ByClassName("uniqeClassName")); //String className aliyor
+        driver.findElement(new By.ByCssSelector("tagname[attributename='value']"));  // String cssSelector aliyor
+        driver.findElement(new By.ByName("uniqeName"));  //String name aliyor
+        driver.findElement(new By.ByLinkText("yazinin tamami")); // Strin olarak baglantı metnini aliyor
+        driver.findElement(new By.ByPartialLinkText("yazinin bir kismi"));
+        driver.findElement(new By.ByTagName("tagValue"));
+        driver.findElement(new By.ByXPath("tagName[@attributeIsmi='attributeValue']"));
+
+        driver.findElement(By.id("fname"));
+        driver.findElement(By.className("uniqeClassName"));
+        driver.findElement(By.cssSelector("tagname[attributename='value']"));
+        driver.findElement(By.name("uniqeName"));
+        driver.findElement(By.linkText("yazinin tamami"));
+        driver.findElement(By.partialLinkText("yazinin bir kismi"));
+        driver.findElement(By.tagName("tagValue"));
+        driver.findElement(By.xpath("tagName[@attributeIsmi='attributeValue']"));
+
+ */
+
+
+
+        driver.findElement(new By.ById("frame"));
+
+
+
+
+
+    }
+}
