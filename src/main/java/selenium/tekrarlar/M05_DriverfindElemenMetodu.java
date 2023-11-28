3b104f272e7a2186588e552358afafee3cfaf7f8
Mehmet Almas <m.almas63@gmail.com>
2023-11-29 00:13:38 +0100

    291123

--- src/main/java/selenium/tekrarlar/M05_DriverfindElemenMetodu.java
+++ src/main/java/selenium/tekrarlar/M05_DriverfindElemenMetodu.java
@@ -1,19 +1,44 @@
 package selenium.tekrarlar;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.Iterator;
+import java.util.Set;
 
 public class M05_DriverfindElemenMetodu {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws InterruptedException {
+
         System.setProperty("webdriver.edge.driver", "src/resources/edgedriver_win64/msedgedriver.exe");
         WebDriver driver = new EdgeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
+;
+        String url = "https://www.zyyah.com/homeowner-lifestyle-perfected-home-value-protected/";
+        driver.get(url);
+        driver.findElement(By.id("//*[@class=\"link link--purple accept-cookies\"]")).click();
+        System.out.println("completed");
 
-        driver.get("https://www.amazon.de/ref=nav_logo");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*
         By ByAll = null;
@@ -40,13 +65,6 @@ public class M05_DriverfindElemenMetodu {
 
  */
 
-
-
-        driver.findElement(new By.ById("frame"));
-
-
-
-
-
     }
+
 }
