d09759fa0fc373a515ba51190015b320f619996b
Mehmet Almas <m.almas63@gmail.com>
2023-12-02 00:02:01 +0100

    021223

--- src/main/java/selenium/tekrarlar/M06_ElementMototlari.java
+++ src/main/java/selenium/tekrarlar/M06_ElementMototlari.java
@@ -23,13 +23,51 @@ public class M06_ElementMototlari {
         cookeiesAkzeptieren.click();
         // driver.manage().deleteAllCookies();
 
-        WebElement searchTextBox =  driver.findElement(By.xpath("//input[@id=\"twotabsearchtextbox\"]"));
+       /*
+        * WebElement searchTextBox =  driver.findElement(By.xpath("//input[@id=\"twotabsearchtextbox\"]"));
+        * searchTextBox.sendKeys("city bike" + Keys.ENTER);
+        * veya searchTextBox.submit();
+        * List<WebElement> ergebnisElement = driver.findElements(By.xpath("//span[contains(text() , '1-16 von 718 Ergebnissen')]"));
+        */
 
-        searchTextBox.sendKeys("city bike" + Keys.ENTER);
-        // veya searchTextBox.submit();
 
 
-        List<WebElement> ergebnisElement = driver.findElements(By.xpath("//span[contains(text() , '1-16 von 718 Ergebnissen')]"));
+        WebElement mehrKaufenundSparen = driver.findElement(By.xpath("//a[@class=\"nav-a  \"][1]"));
+
+        System.out.println("getText()  "  + mehrKaufenundSparen.getText());
+        System.out.println("getTagName()  " + mehrKaufenundSparen.getTagName());
+        System.out.println("getSize()  " + mehrKaufenundSparen.getSize());
+        //System.out.println(mehrKaufenundSparen.getShadowRoot());
+        System.out.println("getRect().getWidth()  "  + mehrKaufenundSparen.getRect().getWidth());
+        System.out.println("getLocation()  " + mehrKaufenundSparen.getLocation());
+        System.out.println("getDomProperty  "  +  mehrKaufenundSparen.getDomProperty("name"));
+        System.out.println("getDomAttribute(\"name\")  " + mehrKaufenundSparen.getDomAttribute("name"));
+        System.out.println("getAttribute(\"name\")  " + mehrKaufenundSparen.getAttribute("name"));
+        System.out.println("getDomAttribute(\"color\")  " + mehrKaufenundSparen.getDomAttribute("color"));
+
+
+        if (mehrKaufenundSparen.isEnabled()) {
+            System.out.println("testPassed");
+        }else{
+            System.out.println("testFailed");
+        }
+
+        if (mehrKaufenundSparen.isSelected()) {
+            System.out.println("testPassed");
+        }else{
+            System.out.println("testFailed");
+        }
+
+        if (mehrKaufenundSparen.isDisplayed()) {
+            System.out.println("testPassed");
+        }else{
+            System.out.println("testFailed");
+        }
+
+        mehrKaufenundSparen.click();
+        // mehrKaufenundSparen.clear(); Text girilen bir alan olmali
+        // mehrKaufenundSparen.submit(); enter tusu gibi
+        // mehrKaufenundSparen.sendKeys("hello"); text girilen bir alan olmali
 
 
 
